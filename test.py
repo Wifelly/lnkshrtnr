4e1c20a646500b8f9c9015b4e24b9206a445be88
NWNZ <nwnzer@gmail.com>
2019-06-24 01:14:44 +0500

    add test

--- test.py
+++ test.py
@@ -1,23 +1,4 @@
-from flask import Flask, request, Response, redirect, jsonify
-from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
-    get_jwt_identity
-)
+from validators import url as validate_url
 
-app = Flask(__name__)
-jwt = JWTManager(app)
-app.config['JWT_SECRET_KEY'] = 'super-secret'
-@app.route('/auth', methods=['POST'])
-def token():
-    data = request.json
-    login = data['login']
-    access_token = create_access_token(identity=login)
-    return jsonify(access_token=access_token), 200
-@app.route('/tk', methods=['GET'])
-def protected():
-    # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
-
-if __name__ == '__main__':
-    app.run()
+url1 = "http://vk.com"
+print(url(url1))
